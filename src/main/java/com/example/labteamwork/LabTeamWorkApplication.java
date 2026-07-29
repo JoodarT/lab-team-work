7a3e6bb7127a267fb8c31d4c0d6e5e0343170eb4
Joodar <joodartynystanov@gmail.com>
2026-07-29 16:05:48 +0600

    init

---
+++ src/main/java/com/example/labteamwork/LabTeamWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.labteamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTeamWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabTeamWorkApplication.class, args);
+    }
+
+}
